6cdf7d4c566fa60e487dbcdf53a01a91d9199644
柴淞 <chaisong.cn@gmail.com>
2016-06-27 13:19:45 +0800

    Initial Commit

---
+++ QiNiuDrop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QiNiuDrop
+//
+//  Created by song on 16/6/27.
+//  Copyright © 2016年 song. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
